2c038bc3e2b82e4484b92ed7a10e2f13d2fcd291
Andrew Walker <andrewwalker1239@gmail.com>
2025-10-05 14:00:42 -0400

    Initial Commit

---
+++ Final/FinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalApp.swift
+//  Final
+//
+//  Created by Andrew Walker on 10/5/25.
+//
+
+import SwiftUI
+
+@main
+struct FinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
